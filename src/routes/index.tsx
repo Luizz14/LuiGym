dfc8883ec3bbc7475e00d19a4a6bc6bef53121b5
LuizPC <gustavo80moura@gmail.com>
2023-09-02 23:44:06 -0300

    Commit aplicando o bottom navigator

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 
-import { AuthRoutes } from './auth.routes'
 import { Box, useTheme } from 'native-base'
+
+import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
 
 export function Routes() {
   const { colors } = useTheme()
@@ -12,7 +14,7 @@ export function Routes() {
   return (
     <Box flex={1} bg={'gray.700'}>
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
