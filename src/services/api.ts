56edae6f42b2dc6f4f324ed346d3b3954bed4726
LuiMac <gustavo80moura@gmail.com>
2023-10-27 15:15:58 -0300

    Refresh_Token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
+import axios, { AxiosError, AxiosInstance } from 'axios'
+
 import { AppError } from '@utils/AppError'
-import axios, { AxiosInstance } from 'axios'
+import { storageTokenGet, storageTokenSave } from '@storage/tokenConfig'
 
 type SignOut = () => void
+
+type PromiseType = {
+  onSuccess: (token: string) => void
+  onFailure: (error: AxiosError) => void
+}
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void
@@ -12,15 +19,87 @@ const api = axios.create({
   baseURL: 'http://172.20.10.2:3333',
 }) as APIInstanceProps
 
+let faliedQueue: Array<PromiseType> = []
+let isRefreshing = false
+
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
     (response) => response,
-    (requestError) => {
+    async (requestError) => {
       if (requestError?.response?.status === 401) {
         if (
           requestError.response.data?.message === 'token.expired' ||
           requestError.response.data?.message === 'token.invalid'
         ) {
+          const { refresh_token } = await storageTokenGet()
+
+          if (!refresh_token) {
+            signOut()
+            return Promise.reject(requestError)
+          }
+
+          const originalRequestConfig = requestError.config
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              faliedQueue.push({
+                onSuccess: (token: string) => {
+                  originalRequestConfig.headers = {
+                    Authorization: `Bearer ${token}`,
+                  }
+                  resolve(api(originalRequestConfig))
+                },
+                onFailure: (error: AxiosError) => {
+                  reject(error)
+                },
+              })
+            })
+          }
+
+          isRefreshing = true
+
+          return new Promise(async (resolve, reject) => {
+            try {
+              const { data } = await api.post('/sessions/refresh-token', {
+                refresh_token,
+              })
+
+              await storageTokenSave({
+                token: data.token,
+                refresh_token: data.refresh_token,
+              })
+
+              if (originalRequestConfig.data) {
+                originalRequestConfig.data = JSON.parse(
+                  originalRequestConfig.data
+                )
+              }
+
+              originalRequestConfig.headers = {
+                Authorization: `Bearer ${data.token}`,
+              }
+
+              api.defaults.headers.common[
+                'Authorization'
+              ] = `Bearer ${data.token}`
+
+              faliedQueue.forEach((request) => {
+                request.onSuccess(data.token)
+              })
+
+              resolve(api(originalRequestConfig))
+            } catch (error: any) {
+              faliedQueue.forEach((request) => {
+                request.onFailure(error)
+              })
+
+              signOut()
+              reject(error)
+            } finally {
+              isRefreshing = false
+              faliedQueue = []
+            }
+          })
         }
 
         signOut()
